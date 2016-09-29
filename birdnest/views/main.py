a8dcf91e958f313148f96fbe2e8a8f4960b22a7c
Tay Frost <taylor@taybird.com>
2016-09-29 11:41:40 -0700

    Make the background rotate on reload

--- birdnest/views/main.py
+++ birdnest/views/main.py
@@ -1,10 +1,30 @@
 from birdnest import app
 
-from flask import redirect, render_template
+
+import random
+
+from flask import make_response
+from flask import redirect
+from flask import render_template
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@pp.route("/background")
+def background():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    image_dir = os.path.abspath(os.path.join(current_dir, "../static/img/")
+    images = glob.glob(os.path.join(image_dir, "bg_*"))
+    background = random.choice(image)
+
+    with open(background) as f:
+        response = make_response(f.read())
+        response.content_type = "image/jpeg"
+
+    return response
+
 
 @app.route("/resume")
 def resume():
