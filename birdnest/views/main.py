858455584bb92f01f18a7437cd1a1922b196b584
Tay Frost <taylor@taybird.com>
2016-11-19 17:03:05 -0800

    Add rotation of background image, plus enable error logging.
    Squashed commit of the following:
    
    commit 9aae4d850a2b3ba2e608d63047a571457d6c5b7c
    Author: Tay Frost <taylor@taybird.com>
    Date:   Sat Nov 19 16:52:44 2016 -0800
    
        bugflix: add glob module import
    
    commit c03038527cd3856f95c1c53cfbbb966526a9aa38
    Author: Tay Frost <taylor@taybird.com>
    Date:   Sat Nov 19 16:49:06 2016 -0800
    
        Bugfix: add os import
    
    commit 5befdd57d539ed8c5cd56de4fb654db53eab1274
    Author: Tay Frost <taylor@taybird.com>
    Date:   Sat Nov 19 16:47:07 2016 -0800
    
        add propagate exceptions config to make debugging easier
    
    commit 8c5dc98d6f312879a055a535ef25ce1b497a831a
    Author: Tay Frost <taylor@taybird.com>
    Date:   Sat Nov 19 16:42:14 2016 -0800
    
        Bugfix: variable name in bg generator
    
    commit b62a80eb854b65fb5549b84cb39413bed7c67565
    Author: Tay Frost <taylor@taybird.com>
    Date:   Sat Nov 19 14:45:38 2016 -0800
    
        fix: import flask.Response
    
    commit 2d1400a0036a8d06e0b5d42969cde36ffc910f66
    Author: Tay Frost <taylor@taybird.com>
    Date:   Sat Nov 19 14:23:52 2016 -0800
    
        Log exceptions when accessing the aboutme project in s3
    
    commit dbd1a4708190acdddc73f46b67f52529b85bf1a9
    Author: Tay Frost <taylor@taybird.com>
    Date:   Sat Nov 19 13:26:57 2016 -0800
    
        Add an entry to server the aboutme project
    
    commit b09c35cc319d24f70d9190f552a0d84fdd53fb30
    Author: Tay Frost <taylor@taybird.com>
    Date:   Thu Sep 29 11:52:19 2016 -0700
    
        Try different rotation solution
    
    commit 690f4ba2893146d8f51e349b7ca57bdafcd71bc5
    Author: Tay Frost <taylor@taybird.com>
    Date:   Thu Sep 29 11:45:55 2016 -0700
    
        Bugfix: app not pp
    
    commit ccc67333803c4b9d7d6042c8c5331fceece01133
    Author: Tay Frost <taylor@taybird.com>
    Date:   Thu Sep 29 11:44:15 2016 -0700
    
        Bugfix: missing paren
    
    commit a8dcf91e958f313148f96fbe2e8a8f4960b22a7c
    Author: Tay Frost <taylor@taybird.com>
    Date:   Thu Sep 29 11:41:40 2016 -0700
    
        Make the background rotate on reload

--- birdnest/views/main.py
+++ birdnest/views/main.py
@@ -1,11 +1,54 @@
 from birdnest import app
 
-from flask import redirect, render_template
+
+import glob
+import logging
+import os
+import random
+
+import boto
+from boto.s3.connection import S3Connection
+from flask import make_response
+from flask import redirect
+from flask import render_template
+from flask import Response
+from flask import send_file
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/background")
+def background():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    image_dir = os.path.abspath(os.path.join(current_dir, "../static/img/"))
+    images = glob.glob(os.path.join(image_dir, "bg_*"))
+    background = random.choice(images)
+
+    with open(background) as f:
+        response = make_response(f.read())
+        response.content_type = "image/jpeg"
+
+    return send_file(background, mimetype="image/jpeg")
+
+
 @app.route("/resume")
 def resume():
 	return redirect("/static/pdf/resume.pdf")
+
+
+@app.route("/aboutme/<path:path>")
+def aboutme(path):
+    # https://technology.jana.com/2015/03/12/using-flask-and-boto-to-create-a-proxy-to-s3/
+    conn = S3Connection(anon=True)
+    bucket = conn.get_bucket('taybird-aboutme', validate=False)
+    key = boto.s3.key.Key(bucket)
+    key.key = path
+
+    try:
+        key.open_read()
+        headers = dict(key.resp.getheaders())
+        return Response(key, headers=headers)
+    except boto.exception.S3ResponseError as e:
+        return flask.Response(e.body, status=e.status, headers=key.resp.getheaders())
