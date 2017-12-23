958cecc36bff2cafd1cbb492a155ea7e8ee1cdc5
Tay Frost <taylor@taybird.com>
2017-12-22 22:07:21 -0800

    A couple of clouds

--- birdnest/js/clouds.js
+++ birdnest/js/clouds.js
@@ -1,5 +1,10 @@
 var canvas = document.getElementById("clouds");
-var ctx = canvas.getContext("2d");
 
-ctx.font='14px FontAwesome';
-ctx.fillText('\uF0C2', canvas.width/2, canvas.height/2);
+function draw_cloud(font_size, x_position, y_position) {
+    var ctx = canvas.getContext("2d");
+    ctx.font= font_size + 'px FontAwesome';
+    ctx.fillText('\uF0C2', x_position, y_position);
+}
+
+draw_cloud(200, canvas.width/2, canvas.height/2)
+draw_cloud(100, canvas.width/4, canvas.height/4)
