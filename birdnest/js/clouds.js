13138f4ebf26003c02aa30f1870eb74ca8414074
Tay Frost <taylor@taybird.com>
2017-12-22 22:28:48 -0800

    Add a resize event

--- birdnest/js/clouds.js
+++ birdnest/js/clouds.js
@@ -1,15 +1,19 @@
-var canvas = document.getElementById("clouds");
+window.addEventListener('resize', resizeCanvas, false);
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+var canvas = document.getElementById("clouds");
+var ctx = canvas.getContext("2d");
+
+function redrawCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    draw_cloud(200, canvas.width/2, canvas.height/2)
+    draw_cloud(100, canvas.width/4, canvas.height/4)
+}
 
 function draw_cloud(font_size, x_position, y_position) {
-    var ctx = canvas.getContext("2d");
-    ctx.font= font_size + 'px FontAwesome';
+    ctx.font = font_size + 'px FontAwesome';
     ctx.fillText('\uF0C2', x_position, y_position);
 }
 
-draw_cloud(200, canvas.width/2, canvas.height/2)
-draw_cloud(100, canvas.width/4, canvas.height/4)
-
-document.body.style.background = "url(" + canvas.toDataURL() + ") no-repeat center center fixed";
+/* document.body.style.background = "url(" + canvas.toDataURL() + ") no-repeat center center fixed"; */
