cdc60de7fe935efefc4a83d22cb36f592b3f8c7b
Tay Frost <taylor@taybird.com>
2017-12-22 23:53:02 -0800

    Adjust the size/pop balance

--- birdnest/js/clouds.js
+++ birdnest/js/clouds.js
@@ -12,7 +12,7 @@ window.onload = function () {
         for (i = 0; i < iterations; i++) {
             // Cloud have a variable size that tends to increase each iteration.
             // Cloud should be larger if total number of clouds is low.
-            first_size = Math.floor(Math.random() * 50) + (i * 25) + (Math.floor(8 - iterations) * 50);
+            first_size = Math.floor(Math.random() * 50) + (i * 25) + (Math.floor(6 - iterations) * 30);
             
             // Cloud tends to be towards the centre, moreso larger ones.
             first_x = Math.floor(Math.random() * (canvas.width - first_size)) + (first_size / 2)
