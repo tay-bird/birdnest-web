1ea9237d6f4196976cb3ab38d2c71bf354c71209
Tay Frost <taylor@taybird.com>
2017-12-22 21:49:53 -0800

    fix variable name

--- birdnest/js/clouds.js
+++ birdnest/js/clouds.js
@@ -2,4 +2,4 @@ var canvas = document.getElementById("clouds");
 var ctx = canvas.getContext("2d");
 
 ctx.font='14px FontAwesome';
-context.fillText('\uF0C2', canvas.width/2, canvas.height/2);
+ctx.fillText('\uF0C2', canvas.width/2, canvas.height/2);
