538d92abe1830946a668735f207576822ce76e56
Tay Frost <taylor@taybird.com>
2017-12-22 23:08:56 -0800

    js shouldn't load until css is fully loaded

--- birdnest/js/clouds.js
+++ birdnest/js/clouds.js
@@ -1,32 +1,36 @@
-var canvas = document.createElement('canvas');
-var ctx = canvas.getContext("2d");
+window.onload = function () {
 
-function redrawCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext("2d");
 
-    iterations = Math.floor(Math.random() * 5) + 5;
+    function redrawCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
 
-    for (i = 0; i < iterations; i++) {
-        first_size = Math.floor(Math.random() * 50) + (i * 25);
-        first_x = Math.floor(Math.random() * (canvas.width - first_size)) + first_size
-        first_y = Math.floor(Math.random() * (canvas.height - first_size)) + first_size
+        iterations = Math.floor(Math.random() * 5) + 5;
 
-        second_size = first_size / i
-        second_x = Math.floor(Math.random() * canvas.width)
-        second_y = Math.floor(Math.random() * canvas.height)
+        for (i = 0; i < iterations; i++) {
+            first_size = Math.floor(Math.random() * 50) + (i * 25);
+            first_x = Math.floor(Math.random() * (canvas.width - first_size)) + first_size
+            first_y = Math.floor(Math.random() * (canvas.height - first_size)) + first_size
 
-        draw_cloud(first_size, first_x, first_y)
-        draw_cloud(second_size, second_x, second_y)
+            second_size = first_size / i
+            second_x = Math.floor(Math.random() * canvas.width)
+            second_y = Math.floor(Math.random() * canvas.height)
+
+            draw_cloud(first_size, first_x, first_y)
+            draw_cloud(second_size, second_x, second_y)
+        }
     }
+
+    function draw_cloud(font_size, x_position, y_position) {
+        ctx.font = font_size + 'px FontAwesome';
+        ctx.fillText('\uF0C2', x_position, y_position);
+    }
+
+    window.addEventListener('resize', redrawCanvas, false);
+    redrawCanvas()
+
+    document.body.style.background = "url(" + canvas.toDataURL() + ") no-repeat center center fixed";
+
 }
-
-function draw_cloud(font_size, x_position, y_position) {
-    ctx.font = font_size + 'px FontAwesome';
-    ctx.fillText('\uF0C2', x_position, y_position);
-}
-
-window.addEventListener('resize', redrawCanvas, false);
-redrawCanvas()
-
-document.body.style.background = "url(" + canvas.toDataURL() + ") no-repeat center center fixed";
