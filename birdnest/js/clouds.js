dd206587faa00e890013569907c4aebdd4cc70fc
Tay Frost <taylor@taybird.com>
2017-12-22 22:22:15 -0800

    Make the canvas fill the page

--- birdnest/js/clouds.js
+++ birdnest/js/clouds.js
@@ -1,5 +1,8 @@
 var canvas = document.getElementById("clouds");
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 function draw_cloud(font_size, x_position, y_position) {
     var ctx = canvas.getContext("2d");
     ctx.font= font_size + 'px FontAwesome';
@@ -8,3 +11,5 @@ function draw_cloud(font_size, x_position, y_position) {
 
 draw_cloud(200, canvas.width/2, canvas.height/2)
 draw_cloud(100, canvas.width/4, canvas.height/4)
+
+document.body.style.background = "url(" + canvas.toDataURL() + ") no-repeat center center fixed";
