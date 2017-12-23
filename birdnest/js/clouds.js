4ae44c4c5e1fb5daa154c7e5039b20f8e8f73514
Tay Frost <taylor@taybird.com>
2017-12-22 23:40:22 -0800

    typo :(

--- birdnest/js/clouds.js
+++ birdnest/js/clouds.js
@@ -24,7 +24,7 @@ window.onload = function () {
             second_y = Math.floor(Math.random() * canvas.height)
             second_alpha = Math.random()- 0.5
 
-            draw_cloud(first_size, first_apha, first_x, first_y)
+            draw_cloud(first_size, first_alpha, first_x, first_y)
             draw_cloud(second_size, second_alpha, second_x, second_y)
         }
     }
