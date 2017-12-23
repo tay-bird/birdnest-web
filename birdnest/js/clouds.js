72d946283ba2b133364ddad74e57625c1d6e0f44
Tay Frost <taylor@taybird.com>
2017-12-22 21:48:02 -0800

    Let's add some clouds

--- birdnest/js/clouds.js
+++ birdnest/js/clouds.js
@@ -1,9 +1,5 @@
 var canvas = document.getElementById("clouds");
 var ctx = canvas.getContext("2d");
 
-ctx.rotate(Math.random() * 2 * Math.PI);
-ctx.fillStyle = "rgb(200,0,0)";
-ctx.fillRect (10, 10, 55, 50);
-
-ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-ctx.fillRect (30, 30, 55, 50);
+ctx.font='14px FontAwesome';
+context.fillText('\uF0C2', canvas.width/2, canvas.height/2);
